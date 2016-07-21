d689c9159b10fda6819e8ff15f0015629eb7aa69
zhangfuqiang <zfq_work@163.com>
2016-07-21 17:39:51 +0800

    CSV

--- CSVTools/CSV_Extractor.py
+++ CSVTools/CSV_Extractor.py
@@ -4,14 +4,13 @@ from CSVTools import *
 
 csv_path = default['csv_path']
 csv = pd.read_csv(csv_path, sep='\t')
-subset = csv[['EPL Segment Description', 'Price Type ']]
+subset = csv[[0, 8, 9, 10, 11]]
 types = []
 for record in subset.values:
     pattern = regex.compile(pattern=r'Maintenance')
     description = record[0]
-    price_type = record[1]
     if pattern.search(description):
-        types.append(price_type)
-types_unique = set(types)
-for t in types_unique:
-    print(t)
+        types.append(record)
+# types_unique = set(types)
+for t in types:
+    print(t, type(t[4]))
