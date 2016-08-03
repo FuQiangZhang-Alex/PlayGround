a5a2277de88ba7857d6b601bdd9c77df16aacc81
zhangfuqiang <zfq_work@163.com>
2016-08-03 17:38:41 +0800

    CSV

--- DSTool/import_export.py
+++ DSTool/import_export.py
@@ -1,18 +1,32 @@
 
 from DSTool import *
+import regex
 import os
 
 host_info = conf['host']
 dir_info = conf['dir']
 cmd_path = conf['sys']['command_path']
 jobs_dir = dir_info['jobs_dir']
-# os.chdir(jobs_dir)
 
 
-def export(job_list_file=None):
+def export(job_list_file=None, begin_line=0, end_line=None):
     cnt = int(conf['dir']['exported'])
     jl = open(file=jobs_list_file, mode='r').readlines()
-    to_process = jl[cnt:]
+    to_process = None
+    if end_line is not None:
+        if cnt > begin_line:
+            to_process = jl[cnt:]
+        else:
+            to_process = jl[begin_line:]
+    else:
+        if end_line > begin_line:
+            print('error')
+            return None
+        if cnt > begin_line & cnt < end_line:
+            to_process = jl[cnt:end_line]
+        else:
+            to_process = jl[begin_line:end_line]
+    print(len(to_process), to_process)
     for job_name in to_process:
         job_name = str(job_name).rstrip('\n')
         job_file_name = job_name + '.dsx'
@@ -34,9 +48,22 @@ def export(job_list_file=None):
 
 
 jobs_list_file = conf['dir']['job_list_file']
-export(jobs_list_file)
+export(jobs_list_file, begin_line=25, end_line=62)
 
 
 def import_(jobs_dir):
     job_names = os.listdir(jobs_dir)
     print(job_names)
+
+
+def list_jobs(folder_name=''):
+    cmd_str = '{0}dsjob -domain {1} -user {2} -password {3} -server {4} -ljobs {5}'\
+        .format(cmd_path, host_info['domain'], host_info['user'],
+                host_info['password'], host_info['host'], host_info['project'])
+    jobs = os.popen(cmd=cmd_str, mode='r')
+    for job_line in jobs.readlines():
+        pattern = regex.compile('^A24T\d[A-Z]{3}$')
+        if pattern.search(job_line):
+            print(job_line.rstrip('\n'))
+
+# list_jobs()
